c9f511b605cb155eef9265689e92534afe1e6564
Sumu154 <sumaiyatasnim5548@gmail.com>
2025-01-15 19:58:52 +0600

    made .env and hide secret data

---
+++ Marathon management-Client side/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-white mt-20 text-sm md:text-base      dark:bg-footerbackground dark:text-white">
+      <div className="text-blacktext py-12 mt-8 max-w-[680px] mx-auto">
+        <h2 className="text-orange font-bold text-center text-base sm:text-2xl"> Crowd Funding </h2>
+        <p className="opacity-70 text-center mx-5"> Uniting runners and enthusiasts worldwide by providing seamless access to marathons and events. </p>
+        <div className="flex flex-col md:flex-row gap-6 md:gap-0 justify-between pt-10">
+          <div className="text-center">
+            <h5 className="text-lg font-bold"> Services </h5>
+            <ul className="opacity-60">
+            <li> <a href="/register"> How to Register for a Marathon </a> </li>
+              <li> <a href="/events"> Upcoming Events </a> </li>
+              <li> <a href="/training"> Training Resources </a> </li>
+              <li> <a href="/contact"> Contact Us </a> </li>
+            </ul>
+          </div>
+          <div className="text-center">
+            <h5 className="text-lg font-bold"> About </h5>
+            <ul className="opacity-60">
+              <li> <a href="/our-mission"> Our Mission </a> </li>
+              <li> <a href="/partners"> Partners & Sponsors </a> </li>
+              <li> <a href="/success-stories"> Success Stories </a> </li>
+            </ul>
+          </div>
+          <div className="text-center">
+            <h5 className="text-lg font-bold"> Legal </h5>
+            <ul className="opacity-60">
+              <li> <a href="/terms"> Terms of Service </a> </li>
+              <li> <a href="/privacy"> Privacy Policy </a> </li>
+              <li> <a href="/donation-policy"> Donation Policy </a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  );
+};
+
+export default Footer;
